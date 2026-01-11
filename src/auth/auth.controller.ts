b8f8bcd347856b9a2af8da25669423550d014cac
Ahmed Sheikh <ahmedos099912@gmail.com>
2026-01-11 11:51:55 +0400

    Adding refresh token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { CurrentUser } from './current-user.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -10,5 +12,16 @@ export class AuthController {
     @Post('login')
     async login (@Body() dto:LoginDto){
         return this.authService.login(dto.email,dto.password)
+    }
+
+    @Post('refresh')
+    async refresh(@Body('refreshToken') refreshToken: string) {
+        return this.authService.refresh(refreshToken);
+    }
+
+    @Post('logout')
+    @UseGuards(AuthGuard('jwt'))
+    logout(@CurrentUser() user) {
+    return this.authService.logout(user.id);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ForbiddenException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt'
+import { PrismaService } from 'src/prisma.service';
 
 
 
@@ -9,7 +10,8 @@ import * as bcrypt from 'bcrypt'
 export class AuthService {
     constructor(
         private readonly userService:UsersService,
-        private readonly jwtService:JwtService
+        private readonly jwtService:JwtService,
+        private readonly prisma:PrismaService
     ){}
 
 
@@ -28,17 +30,71 @@ export class AuthService {
     async login(email:string,password:string){
         const user = await this.validateUser(email,password)
 
-        const payload = {sub:user.id,email:user.email,role:user.role}
-        const token = this.jwtService.sign(payload)
+        const tokens = await this.getTokens(user.id,user.email)
+
+        await this.updateRefreshToken(user.id,await tokens.refreshToken)
 
         return {
-            assess_token:token,
+            assess_token:tokens.accessToken,
+            refreshtoken:tokens.refreshToken,
             user:{
                 id:user.id,
                 role:user.role,
                 email:user.email
             }
         }
+    }
+
+    private async getTokens(userId: string, email: string) {
+        const payload = { sub: userId, email };
+
+        const accessToken = this.jwtService.sign(payload, {
+            expiresIn: '15m',
+        });
+
+        const refreshToken = this.jwtService.sign(payload, {
+            expiresIn: '7d',
+        });
+
+        return { accessToken, refreshToken };
+        }
+
+
+    private async updateRefreshToken(userId: string, refreshToken: string) {
+        const hashed = await bcrypt.hash(refreshToken, 10);
+
+        await this.prisma.user.update({
+            where: { id: userId },
+            data: { refreshToken: hashed },
+        });
+    }
+
+    async refresh(refreshToken: string) {
+        const payload = this.jwtService.verify(refreshToken);
+
+        const user = await this.prisma.user.findUnique({
+            where: { id: payload.sub },
+        });
+
+        if (!user || !user.refreshToken)
+            throw new ForbiddenException();
+
+        const match = await bcrypt.compare(refreshToken, user.refreshToken);
+        if (!match) throw new ForbiddenException();
+
+        const tokens = await this.getTokens(user.id, user.email);
+        await this.updateRefreshToken(user.id, tokens.refreshToken);
+
+        return tokens;
+        }
+        
+    async logout(userId: string) {
+        await this.prisma.user.update({
+            where: { id: userId },
+            data: { refreshToken: null },
+        });
+
+        return { message: 'Logged out' };
     }
 }
 
